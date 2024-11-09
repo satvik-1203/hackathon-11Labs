668ad6471dc46737b4ad64901138b163f39647c4
justinkook <justinkook@gmail.com>
2024-11-09 15:24:31 -0500

    feat: port 8080

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 
